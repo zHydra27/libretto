6213ac919de4d24b1139ec039b2b824f1482dfe3
zHydra27 <france.came27@gmail.com>
2026-09-08 17:54:00 +0200

    Change Supabase URL to a new publishable key
    Updated the Supabase URL to a new value.

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const SUPABASE_URL = "https://mxtkefwcyajcncwipjdm.supabase.co"; // Il tuo URL
+const SUPABASE_URL = "sb_publishable_UpTpWQyV8qt8iAT-ubVmAg_yW1AP8eW"; // Il tuo URL
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14dGtlZndjeWFqY25jd2lwamRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1OTc2NTUsImV4cCI6MjEwNDE3MzY1NX0.xV96xteOEeVhNFWRKamSYeqacOzBjQqeL07nep9a7uc"; // La tua chiave
 export const isSupabaseConfigured = !SUPABASE_URL.includes("IL-TUO-PROGETTO") && !SUPABASE_ANON_KEY.startsWith("INCOLLA");
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
